d4515e58c7142ee6899cc564bf29b36c2605e414
Steven <steven.guan.wang@gmail.com>
2019-09-22 22:09:05 +1000

    Updates

---
+++ precache-manifest.9b6713a2ca52328714a05fc639af2f68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ff4144ffafc6effe7a1b4f991ab9d2",
+    "url": "/city-info-react/index.html"
+  },
+  {
+    "revision": "4b466fcbb7bd050e4752",
+    "url": "/city-info-react/static/css/2.e71d8db5.chunk.css"
+  },
+  {
+    "revision": "9298dc8ba5a16e2a6f51",
+    "url": "/city-info-react/static/css/main.4d362aef.chunk.css"
+  },
+  {
+    "revision": "4b466fcbb7bd050e4752",
+    "url": "/city-info-react/static/js/2.93bb1398.chunk.js"
+  },
+  {
+    "revision": "9298dc8ba5a16e2a6f51",
+    "url": "/city-info-react/static/js/main.0503698b.chunk.js"
+  },
+  {
+    "revision": "bb4082c38f5d1268db4c",
+    "url": "/city-info-react/static/js/runtime-main.838f7773.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/city-info-react/static/media/logo.25bf045c.svg"
+  }
+]);
